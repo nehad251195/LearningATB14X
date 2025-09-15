2895d2ac4312e7602ca89b35cd9a6298cf7cad87
neha.dorugade <neha.dorugade@perfios.com>
2025-09-15 16:28:21 +0530

    PrintF_Execrise

---
+++ src/Lab0033_printF_exercise.java
@@ -0,0 +1,17 @@
+public class Lab0033_printF_exercise {
+    public static void main(String[] args) {
+        int i=9;
+
+        System.out.printf("%d * 1= %d\n" ,i,i*1);
+        System.out.printf("%d * 1= %d\n" ,i,i*2);
+        System.out.printf("%d * 1= %d\n" ,i,i*3);
+        System.out.printf("%d * 1= %d\n" ,i,i*4);
+        System.out.printf("%d * 1= %d\n" ,i,i*5);
+        System.out.printf("%d * 1= %d\n" ,i,i*6);
+        System.out.printf("%d * 1= %d\n" ,i,i*7);
+        System.out.printf("%d * 1= %d\n" ,i,i*8);
+        System.out.printf("%d * 1= %d\n" ,i,i*9);
+        System.out.printf("%d * 1= %d\n" ,i,i*10);
+
+    }
+}
